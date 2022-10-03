1753acc9c998c32a262486459e2e0ac21c86281c
JuaAcha <achilhamm.a@gmail.com>
2022-10-02 19:43:42 -0700

    sia

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,88 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <a href="../../index3.html" class="brand-link">
+        <img src="https://github.com/JuaAcha.png" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">SIA's Project</span>
+    </a>
+
+    <div class="sidebar">
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="https://github.com/JuaAcha.png" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">𝐘𝐮𝐧𝐚૮₍ ˃ ⤙ ˂ ₎ა</a>
+            </div>
+        </div>
+
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                    aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+
+                <li class="nav-header">Dashboard</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ request()->is('dashboard*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-header">Master</li>
+                <li class="nav-item">
+                    <a href="{{route('guru.index') }}" class="nav-link {{ request()->is('guru*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-user-alt"></i>
+                        <p>
+                            Guru
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('kelas.index') }}" class="nav-link {{ request()->is('kelas*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-chalkboard"></i>
+                        <p>
+                            Kelas
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('mapel.index') }}" class="nav-link {{ request()->is('mapel*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-book"></i>
+                        <p>
+                            Mapel
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('siswa.index') }}" class="nav-link {{ request()->is('siswa*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-user-graduate"></i>
+                        <p>
+                            Siswa
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+
+</aside>
